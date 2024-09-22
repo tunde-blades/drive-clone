17cd25480359bd6eab26d18c3a0d1860640c6dee
Micheal Oluwasheyi <oluwasheyidavidmicheal@gmail.com>
2024-09-22 04:38:23 +0100

    updated

--- script.js
+++ script.js
@@ -20,18 +20,26 @@ closenav.addEventListener('click',()=>{
 
 let q1 = document.querySelector('.q1');
 let a1 = document.querySelector('.a1');
+let i1 = document.querySelector('.i1');
 
 let q2 = document.querySelector('.q2');
 let a2 = document.querySelector('.a2');
+let i2 = document.querySelector('.i2');
 
 let q3 = document.querySelector('.q3');
 let a3 = document.querySelector('.a3');
+let i3 = document.querySelector('.i3');
 
 let q4 = document.querySelector('.q4');
 let a4 = document.querySelector('.a4');
+let i4 = document.querySelector('.i4');
 
 let q5 = document.querySelector('.q5');
 let a5 = document.querySelector('.a5');
+let i5 = document.querySelector('.i5');
+
+
+
 
 q1.addEventListener('click', ()=>{
 
@@ -42,6 +50,11 @@ q1.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '0deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
         
     } else {
         a1.style.display = 'block';
@@ -49,6 +62,11 @@ q1.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '180deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
     }
 
 
@@ -63,6 +81,11 @@ q2.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '0deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
         
     } else {
         a2.style.display = 'block';
@@ -70,6 +93,11 @@ q2.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i2.style.rotate = '180deg'
+        i1.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
     }
 
 })
@@ -83,6 +111,11 @@ q3.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '0deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
         
     } else {
         a3.style.display = 'block';
@@ -90,6 +123,11 @@ q3.addEventListener('click', ()=>{
         a2.style.display = 'none';
         a5.style.display = 'none';
         a4.style.display = 'none';
+        i3.style.rotate = '180deg'
+        i2.style.rotate = '0deg'
+        i1.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
 
     }
 
@@ -103,6 +141,11 @@ q4.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '0deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
         
     } else {
         a4.style.display = 'block';
@@ -110,6 +153,11 @@ q4.addEventListener('click', ()=>{
         a2.style.display = 'none';
         a5.style.display = 'none';
         a3.style.display = 'none';
+        i4.style.rotate = '180deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i1.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
     }
 
 })
@@ -122,6 +170,11 @@ q5.addEventListener('click', ()=>{
         a5.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i1.style.rotate = '0deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i5.style.rotate = '0deg'
         
     } else {
         a5.style.display = 'block';
@@ -129,6 +182,11 @@ q5.addEventListener('click', ()=>{
         a1.style.display = 'none';
         a4.style.display = 'none';
         a3.style.display = 'none';
+        i5.style.rotate = '180deg'
+        i2.style.rotate = '0deg'
+        i3.style.rotate = '0deg'
+        i4.style.rotate = '0deg'
+        i1.style.rotate = '0deg'
     }
 
 })
